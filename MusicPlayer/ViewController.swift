e6965da631547afa1c39ca290cd94cffbc454acf
Ellie <ecjeong@maxst.com>
2021-03-18 15:20:41 +0900

    create project

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by 정은채 on 2021/03/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
